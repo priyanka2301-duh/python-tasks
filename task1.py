2d5dde295b609b04cc9862bfacb61502da54fd2a
priyanka2301-duh <84245880+priyanka2301-duh@users.noreply.github.com>
2021-05-29 17:03:27 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/dhivya.g/AppData/Local/Programs/Python/Python36-32/task 1.py 
+Input the radius of the circle : 1.1
+the area of the circle with radius 1.1 is: 3.8013271108436504
+>>> 
+ RESTART: C:/Users/dhivya.g/AppData/Local/Programs/Python/Python36-32/task1.2.py 
+Input the Filename: abc.python
+The extension of the file is : 'python'
+>>> 
