ad765c71dc9a0626fd5188f50460dbd9f2349307
priyanka2301-duh <84245880+priyanka2301-duh@users.noreply.github.com>
2021-05-30 23:45:44 +0530

    Add files via upload

---
+++ task 1.2(+).py
@@ -0,0 +1,9 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/dhivya.g/AppData/Local/Programs/Python/Python36-32/task.3.py 
+12 5 64 
+>>> 
+ RESTART: C:/Users/dhivya.g/AppData/Local/Programs/Python/Python36-32/task.3.py 
+12 14 3 
+>>> 
---
+++ task1.1.py
@@ -0,0 +1,7 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\dhivya.g\AppData\Local\Programs\Python\Python36-32\task2.py 
+Enter the value of 'n': 5
+Fibonacci series:  0 1 1 2 3 
+>>> 
