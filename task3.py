2bde7975e6e392d02607730fc9fa9e860fafe6f6
priyanka2301-duh <84245880+priyanka2301-duh@users.noreply.github.com>
2021-06-02 20:12:43 +0530

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,11 @@
+Python 3.6.8 (tags/v3.6.8:3c6b436a57, Dec 23 2018, 23:31:17) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/dhivya.g/AppData/Local/Programs/Python/Python36-32/task3.0.py 
+Input a string(Word):Mississippi
+the unique letters from this word are: ['s', 'i', 'M', 'p']
+s= 4
+i= 4
+p= 2
+M= 0
+>>> 
